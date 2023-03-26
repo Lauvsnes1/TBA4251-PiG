c65d43c3cf955f3417532d52cc0bf9a7f0ac0367
Even Lauvsnes <even.lauvsnes99@gmail.com>
2023-03-26 14:52:08 -0500

    finally fixed gap

--- src/components/styledComponents.tsx
+++ src/components/styledComponents.tsx
@@ -1,29 +1,7 @@
 import React, { useState,} from 'react';
 import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import PaletteIcon from '@mui/icons-material/Palette';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import Popper from '@mui/material/Popper';
-import Fade from '@mui/material/Fade';
-import Stack from '@mui/material/Stack';
+
 
 const drawerWidth = 240; 
 
@@ -53,7 +31,7 @@ export const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open
     open?: boolean;
   }>(({ theme, open }) => ({
     flexGrow: 1,
-    padding: theme.spacing(3),
+    //padding: theme.spacing(3),
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
