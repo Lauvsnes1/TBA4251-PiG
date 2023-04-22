753ce0368709fe8462e944ba18c9859a22671d24
Even Lauvsnes <even.lauvsnes99@gmail.com>
2023-04-22 17:31:22 -0500

    draw mvp

--- src/components/baseMapMapbox.tsx
+++ src/components/baseMapMapbox.tsx
@@ -87,20 +87,23 @@ function BaseMap() {
           const geoJSONSource = map.getSource(layer.id) as mapboxgl.GeoJSONSource;
           geoJSONSource.setData(layer.geoJSON);
           console.log('layer processing:', layer);
-          switch (type) {
-            case 'fill':
-              map.setPaintProperty(layer.id, 'fill-color', layer.color);
-              map.setPaintProperty(layer.id, 'fill-opacity', layer.opacity);
-              break;
-            case 'line':
-              map.setPaintProperty(layer.id, 'line-color', layer.color);
-              //map.setPaintProperty(layer.id, 'line-opacity', layer.opacity);
-              break;
-            case 'circle':
-              map.setPaintProperty(layer.id, 'circle-color', layer.color);
-              //map.setPaintProperty(layer.id, 'circle-opacity', layer.opacity);
-              break;
-          }
+          console.log('maps style', map.getStyle());
+          map.once('sourcedata', () => {
+            switch (type) {
+              case 'fill':
+                map.setPaintProperty(layer.id, 'fill-color', layer.color);
+                map.setPaintProperty(layer.id, 'fill-opacity', layer.opacity);
+                break;
+              case 'line':
+                map.setPaintProperty(layer.id, 'line-color', layer.color);
+                //map.setPaintProperty(layer.id, 'line-opacity', layer.opacity);
+                break;
+              case 'circle':
+                map.setPaintProperty(layer.id, 'circle-color', layer.color);
+                //map.setPaintProperty(layer.id, 'circle-opacity', layer.opacity);
+                break;
+            }
+          });
         }
         map.setLayoutProperty(layer.id, 'visibility', determineVisibility(layer));
       });
@@ -119,8 +122,8 @@ function BaseMap() {
       // Update the map with the new layer name
       if (map) {
         // Check if the layer exists before removing it
-        if (map?.getLayer(selectedLayer.name)) {
-          map?.removeLayer(selectedLayer.name);
+        if (map?.getLayer(selectedLayer.id)) {
+          map?.removeLayer(selectedLayer.id);
         }
       }
       closeEditModal();
@@ -214,15 +217,28 @@ function BaseMap() {
             opacity: 0.5,
             geoJSON: data as FeatureCollection,
           };
+          map?.addSource(newObj.id, {
+            type: 'geojson',
+            data: newObj.geoJSON,
+          });
+          map?.addLayer({
+            id: newObj.id,
+            type: 'fill',
+            source: newObj.id,
+            paint: { 'fill-color': newObj.color, 'fill-opacity': newObj.opacity } as FillPaint,
+          });
+          console.log('ADDED');
+
           setGeoJSONList((prevGeoJSONs: GeoJSONItem[]) => [...prevGeoJSONs, newObj as GeoJSONItem]);
-          handleShowEditModal();
-          setSelectedLayer(newObj);
+
+          //handleShowEditModal();
+          //setSelectedLayer(newObj);
         }
       };
 
       mapInit.addControl(draw, 'bottom-left');
       mapInit.on('draw.create', createDrawing);
-      //mapInit.on('draw.update', createDrawing);
+      mapInit.on('draw.update', createDrawing);
 
       setMap(mapInit);
       mapRef.current = mapInit;
@@ -264,132 +280,6 @@ function BaseMap() {
     };
     updateLayers();
   }, [geoJSONList]);
-
-  // useEffect(() => {
-  //   if (!mapRef.current) {
-  //     return;
-  //   }
-
-  //   const map = mapRef.current;
-
-  //   const layerIdsToRemove: string[] = [];
-  //   const layerNamesToRemove: string[] = [];
-  //   const styleLayers = mapRef.current.getStyle().layers || [];
-
-  //   map.on('load', () => {
-  //     map.getStyle().layers?.forEach((layer) => {
-  //       const layerIndex = geoJSONList.findIndex((geoJSONItem) => geoJSONItem.name === layer.id);
-  //       if (layerIndex === -1) {
-  //         layerIdsToRemove.push(layer.id as string);
-  //         layerNamesToRemove.push(layer.id as string);
-  //       }
-  //     });
-  //   });
-
-  //   if (layerIdsToRemove.length > 0) {
-  //     layerIdsToRemove.forEach((layerId, index) => {
-  //       const layerName = layerNamesToRemove[index];
-  //       map.removeLayer(layerName);
-  //       map.removeSource(layerId);
-  //     });
-  //   }
-  // }, [geoJSONList]);
-
-  // useEffect(() => {
-  //   if (!mapContainer.current) {
-  //     return;
-  //   }
-
-  //   const map = new mapboxgl.Map({
-  //     container: mapContainer.current,
-  //     style: baseMap,
-  //     center: [lng, lat],
-  //     zoom: zoom,
-  //   });
-
-  //   const draw = new MapboxDraw({
-  //     displayControlsDefault: false,
-  //     // Select which mapbox-gl-draw control buttons to add to the map.
-  //     controls: {
-  //       polygon: true,
-  //       trash: true,
-  //     },
-  //   });
-  //   const createDrawing = () => {
-  //     const data = draw.getAll();
-  //     if (data.features.length > 0) {
-  //       const newObj: GeoJSONItem = {
-  //         id: uid(),
-  //         name: uid(),
-  //         visible: true,
-  //         color: generateColor(),
-  //         opacity: 0.5,
-  //         geoJSON: data as FeatureCollection,
-  //       };
-  //       setGeoJSONList((prevGeoJSONs: GeoJSONItem[]) => [...prevGeoJSONs, newObj as GeoJSONItem]);
-  //       handleShowEditModal();
-  //       setSelectedLayer(newObj);
-  //     }
-  //   };
-
-  //   map.addControl(draw, 'bottom-left');
-  //   map.on('draw.create', createDrawing);
-  //   map.on('draw.update', createDrawing);
-
-  //   map.on('load', () => {
-  //     geoJSONList.forEach((layer) => {
-  //       map.addSource(layer.id, {
-  //         type: 'geojson',
-  //         data: layer.geoJSON,
-  //       });
-
-  //       const { type, paint } = determineType(layer);
-
-  //       switch (type) {
-  //         case 'fill':
-  //           map.addLayer({
-  //             id: layer.name,
-  //             type: type,
-  //             source: layer.id,
-  //             paint: paint as FillPaint,
-  //           });
-  //           break;
-  //         case 'circle':
-  //           map.addLayer({
-  //             id: layer.name,
-  //             type: type,
-  //             source: layer.id,
-  //             paint: paint as CirclePaint,
-  //           });
-  //           break;
-  //         case 'line':
-  //           map.addLayer({
-  //             id: layer.name,
-  //             type: type,
-  //             source: layer.id,
-  //             paint: paint as LinePaint,
-  //           });
-  //           break;
-  //         default:
-  //           throw new Error(`Unsupported layer type: ${type}`);
-  //       }
-  //       map.setLayoutProperty(layer.name, 'visibility', determineVisibility(layer));
-  //     });
-  //   });
-
-  //   //to keep persistent position
-  //   map.on('move', () => {
-  //     setLng(Number(map.getCenter().lng.toFixed(4)));
-  //     setLat(Number(map.getCenter().lat.toFixed(4)));
-  //     setZoom(Number(map.getZoom().toFixed(2)));
-  //   });
-
-  //   return () => {
-  //     map.remove();
-  //   };
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [geoJSONList, baseMap]);
 
   return (
     <div>
